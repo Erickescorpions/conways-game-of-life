3f4fa51f10b4a95ce00f778db025b4eb4512608b
Erick Vazquez <erickescorpions@comunidad.unam.mx>
2023-08-22 15:45:45 -0600

    agregando avance a the game of ley.

---
+++ main.py
@@ -0,0 +1,10 @@
+
+class Board:
+    
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+
+def random_state(width: int, height: int) -> Board:
+    pass
